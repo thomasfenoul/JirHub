15091d3cfbf329c288b274c2c279f08fd87d56ae
Lucas Gérard <lucas.gerard.web@gmail.com>
2018-12-11 14:44:08 +0100

    Test3

--- src/Handler/GitHubHandler.php
+++ src/Handler/GitHubHandler.php
@@ -197,12 +197,11 @@ class GitHubHandler
             return 'OK';
         }
 
-        dump(
-            preg_match(getenv('GITHUB_HEAD_BRANCH_REGEX_PATTERN'), $headBranchName),
-            $pullRequest['base']['ref'] === getenv('GITHUB_DEFAULT_BASE_BRANCH'),
-            true === $force,
-            $this->hasLabel($pullRequest, getenv('GITHUB_FORCE_LABEL'))
-        );
+        echo
+            preg_match(getenv('GITHUB_HEAD_BRANCH_REGEX_PATTERN'), $headBranchName) . ' - ' .
+            $pullRequest['base']['ref'] === getenv('GITHUB_DEFAULT_BASE_BRANCH')  . ' - ' .
+            (true === $force)  . ' - ' .
+            $this->hasLabel($pullRequest, getenv('GITHUB_FORCE_LABEL'));
 
         if (empty($pullRequest) || null === $pullRequest) {
             echo 'We have not found any pull request with head branch "' . $headBranchName . '".';
