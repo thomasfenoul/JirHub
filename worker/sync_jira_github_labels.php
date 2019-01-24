0e27ff30daf6fc4a54ba5cfb41b82e3437ff02da
Lucas Gérard <lucas.gerard.web@gmail.com>
2019-01-24 14:42:02 +0100

    fix wip

--- worker/sync_jira_github_labels.php
+++ worker/sync_jira_github_labels.php
@@ -20,8 +20,7 @@ $gitHubHandler->mergePullRequest($pr, 'squash');
 
 while (true) {
     try {
-        $gitHubHandler->addValidationRequiredLabels();
-        $gitHubHandler->organizeJiraIssues();
+        $gitHubHandler->synchronize();
     } catch (\Exception $e) {
         $worker->getLogger()->error('Error occured during sync_jira_github_labels process : ' . $e->getMessage());
     }
