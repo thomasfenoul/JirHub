d1f7538b40c87065ca475c6a16f45680c5d7a7bf
jonbk <jon.buyck@outlook.fr>
2022-05-13 16:21:12 +0200

    reborn

---
+++ src/Command/RecordAMQPMetricsCommand.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Command;
+
+use App\Repository\AMQP\AMQPQueueMetricsRepository;
+use Elasticsearch\Client;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class RecordAMQPMetricsCommand extends Command
+{
+    /** @var string */
+    protected static $defaultName = 'record:amqp-metrics';
+
+    private AMQPQueueMetricsRepository $AMQPQueueMetricsRepository;
+    private Client $elasticsearchClient;
+    private string $index;
+
+    public function __construct(AMQPQueueMetricsRepository $AMQPQueueMetricsRepository, Client $elasticsearchClient, string $index)
+    {
+        parent::__construct();
+
+        $this->AMQPQueueMetricsRepository = $AMQPQueueMetricsRepository;
+        $this->elasticsearchClient        = $elasticsearchClient;
+        $this->index                      = $index;
+    }
+
+    protected function configure()
+    {
+        $this->setDescription('Record all AMQP queues metrics');
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output): void
+    {
+        $params = ['body' => []];
+        $metrics = $this->AMQPQueueMetricsRepository->getQueuesMetrics();
+
+        foreach ($metrics as $metric){
+            $params['body'][] = [
+                'index' => [
+                    '_index' => $this->index,
+                ]
+            ];
+
+            $params['body'][] = $metric;
+        }
+
+        $this->elasticsearchClient->bulk($params);
+    }
+}
